7665e96b3c5da234759219d54618b0c7a9062fe0
ajayvenkat10 <37923623+ajayvenkat10@users.noreply.github.com>
2020-05-26 18:47:05 +0530

    Test sambar stories

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from lib import *
-from flask import Flask,request
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -7,25 +7,30 @@ SHOPIFY_ORDERS = "Shopify Orders File"
 PAYU_TRANSACTIONS = "PayU Transactions File"
 ABANDONED_CART = "Shopify Abandoned Cart File"
 
-@app.route('/', methods = ['POST'])
+
+@app.route('/')
+def index():
+    return "sambar stories"
+
+
+@app.route('/process', methods=['POST'])
 def process():
     request_json = request.get_json()
-    responses = request_json["responses"]    
-    
+    responses = request_json["responses"]
+
     shopify_orders = ""
     payu_trans = ""
     abandoned_carts = ""
 
     for response in responses:
         if(response["title"] == SHOPIFY_ORDERS):
-            shopify_orders = response["response"]     
+            shopify_orders = response["response"]
 
         if(response["title"] == PAYU_TRANSACTIONS):
-            payu_trans = response["response"]     
+            payu_trans = response["response"]
 
         if(response["title"] == ABANDONED_CART):
-            abandoned_carts = response["response"]     
+            abandoned_carts = response["response"]
 
     return process_inputs(get_csv_file_from_url(payu_trans), get_csv_file_from_url(shopify_orders),
-                         get_csv_file_from_url(abandoned_carts))
-    
+                          get_csv_file_from_url(abandoned_carts))
