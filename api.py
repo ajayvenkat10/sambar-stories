c0e0952a113f97729525592f00eccf8a2168ed6b
Sankar Mahadevan <sankar@rippling.com>
2020-05-28 22:47:49 +0530

    Fix email param

--- api.py
+++ api.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 SHOPIFY_ORDERS = "Shopify Orders File"
 PAYU_TRANSACTIONS = "PayU Transactions File"
 ABANDONED_CART = "Shopify Abandoned Cart File"
-EMAIL_ADDRESS = "Email address"
 
 @app.route('/')
 def index():
@@ -18,11 +17,12 @@ def process():
     request_json = request.get_json()
     print(request_json)
     responses = request_json["responses"]
+    to_address_email = request_json["email"]
     print(responses)
     shopify_orders = ""
     payu_trans = ""
     abandoned_carts = ""
-    to_address_email = ""
+    
 
     for response in responses:
         if(response["title"] == SHOPIFY_ORDERS):
@@ -33,9 +33,6 @@ def process():
 
         if(response["title"] == ABANDONED_CART):
             abandoned_carts = response["response"]
-
-        if(response["title"] == EMAIL_ADDRESS):
-            to_address_email = response["response"]
     
     result_file, row_count = process_inputs(get_csv_file_from_url(payu_trans), get_csv_file_from_url(shopify_orders),
                                  get_csv_file_from_url(abandoned_carts))
